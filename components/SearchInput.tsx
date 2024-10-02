603298e98588518ca1ff9fc5d625f370fc2d6f43
Yasas Lakmina <138884690+YasasLakmina@users.noreply.github.com>
2024-10-02 16:05:48 +0530

    Home page part 1

---
+++ components/SearchInput.tsx
@@ -0,0 +1,50 @@
+import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+import React, { useState } from "react";
+import { icons } from "../constants";
+
+interface SearchInputProps {
+  title: string;
+  value: string;
+  placeholder: string;
+  handleChangeText: (text: string) => void;
+  otherStyles?: string;
+  [key: string]: any;
+}
+
+const SearchInput: React.FC<SearchInputProps> = ({
+  title,
+  value,
+  placeholder,
+  handleChangeText,
+  otherStyles,
+  ...props
+}) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center flex-row space-x-4">
+      <TextInput
+        className="text-base mt-0.5 text-white flex-1 font-pregular"
+        value={value}
+        placeholder={placeholder}
+        placeholderTextColor="#7b7b8b"
+        onChangeText={handleChangeText}
+        secureTextEntry={title === "Password" && !showPassword}
+      />
+      {title === "Password" && (
+        <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+          <Image
+            source={!showPassword ? icons.eye : icons.eyeHide}
+            className="w-6 h-6 "
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+      )}
+      <TouchableOpacity>
+        <Image source={icons.search} className="w-5 h-5" resizeMode="contain" />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default SearchInput;
